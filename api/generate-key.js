25e79cfc50fc4a7919c85c2000a5425885ba2500
PrivateGui <110562273+PrivateGui@users.noreply.github.com>
2025-06-01 09:32:14 +0330

    Update generate-key.js

--- api/generate-key.js
+++ api/generate-key.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../lib/mongodb";
+import clientPromise from "../lib/mongodb";
 
 function generateRandomKey(length = 16) {
   return [...Array(length)].map(() => Math.random().toString(36)[2]).join("");
